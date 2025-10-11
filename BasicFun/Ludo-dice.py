6a41da603c15090103e0e87dce2ea87e54e02094
Abdul Quadir <54887629+CodewithKyu@users.noreply.github.com>
2025-10-12 01:55:53 +0530

    Create Ludo-dice.py

---
+++ BasicFun/Ludo-dice.py
@@ -0,0 +1,9 @@
+#Ludo dice 
+
+#The randint() function is a part of Python's random module, used to generate a random integer within a specified range
+
+import random
+
+# random.randint(lower bound, upper bound)
+r = random.randint(1,6)
+print(r)
